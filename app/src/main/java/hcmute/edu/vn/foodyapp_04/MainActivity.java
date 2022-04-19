24543486f9071a9e88c7c591e694b5f91bf8dd18
An Le <thienan12359@gmail.com>
2022-04-20 03:53:51 +0700

    Create UI

--- app/src/main/java/hcmute/edu/vn/foodyapp_04/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/foodyapp_04/MainActivity.java
@@ -1,21 +1,60 @@
 package hcmute.edu.vn.foodyapp_04;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+    private
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        bottomNavigationView = findViewById(R.id.bottomNAVView);
+        configureNAVBar();
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLayout, new HomeFragment()).commit();
     }
+    private void configureNAVBar(){
+        bottomNavigationView.setBackground(null);
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new
+            BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()){
+                        case  R.id.miHome:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.miSupport:
+                            selectedFragment = new SupportFragment();
+                            break;
+                        case R.id.miNotification:
+                            selectedFragment = new NotificationFragment();
+                            break;
+                        case R.id.miProfile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentLayout
+                            ,selectedFragment).commit();
+                    return true;
+                }
+            };
 
 
 }
